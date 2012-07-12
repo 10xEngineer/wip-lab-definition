3230ebdcc05a4caaf906b85b5830237f484f9c06
Radim Marek <radim@laststation.net>
2012-07-12 22:42:33 +0200

    Added Chef handler

--- metadata.rb
+++ metadata.rb
@@ -1,7 +1,7 @@
 maintainer "Radim Marek"
 maintainer "radim@10xengineer.me"
 
-provider "TenxLabs::Chef::Provider"
+use "TenxLabs::ChefHandler"
 
 version "0.0.1"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
