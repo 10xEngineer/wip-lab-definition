3680af17c9b9e25b249b0202445c627958566a0a
Radim Marek <radim@laststation.net>
2012-07-12 22:41:45 +0200

    Added definition handler

--- metadata.rb
+++ metadata.rb
@@ -1,5 +1,7 @@
 maintainer "Radim Marek"
 maintainer "radim@10xengineer.me"
 
+provider "TenxLabs::Chef::Provider"
+
 version "0.0.1"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
