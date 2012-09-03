06a2a28baf8091837e0eae6db0a89992b48c3240
Radim Marek <radim@laststation.net>
2012-09-03 09:43:59 +0200

    Resource Pool definition

--- metadata.rb
+++ metadata.rb
@@ -3,5 +3,10 @@ maintainer_email "radim@10xengineer.me"
 
 use "TenxLabs::ChefHandler"
 
+# resource pools are bound to lab/component level
+# granular resource allocation possible via component composition
+# TODO resource pool auto-disovery based on resource pool characteristics?
+resource_pool :compute, :default
+
 version "0.0.1"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
